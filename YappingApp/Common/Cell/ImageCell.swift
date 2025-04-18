1277ff246d3e55090a3af594d39c8aafdc4dc734
Longvu <longvu1217@gmail.com>
2025-04-19 00:55:59 +0700

    up

---
+++ YappingApp/Common/Cell/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  YappingApp
+//
+//  Created by Long Vu on 19/4/25.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
