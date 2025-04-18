e18091282c2af9773195f019da1a16f3c4e4ae2a
Longvu <longvu1217@gmail.com>
2025-04-19 00:56:13 +0700

    up

--- YappingApp/Screens/Image/ImageViewController.swift
+++ YappingApp/Screens/Image/ImageViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class ImageViewController: BaseViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    setupCollectionView()
+  }
+  
+  private func setupCollectionView() {
+    
+    
+  }
 }
