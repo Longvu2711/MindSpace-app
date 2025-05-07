253b2595512156e416607f699d8c519271012582
Longvu <longvu1217@gmail.com>
2025-05-07 19:09:48 +0700

    discord connected

--- YappingApp/Common/Constants.swift
+++ YappingApp/Common/Constants.swift
@@ -8,10 +8,5 @@
 import Foundation
 
 struct Constants {
-  static let discordWebhookURL: String = {
-    guard let url = Bundle.main.infoDictionary?["DISCORD_WEBHOOK_URL"] as? String else {
-      fatalError("DISCORD_WEBHOOK_URL failed")
-    }
-    return url
-  }()
+  static let discordWebhookURL = "https://discord.com/api/webhooks/1360257481346912256/om6xn7d2Nhe43_Tyxl8dL2fZLOAGA9smCbkG07m4PHrvNx8kXhebE2gCj694bZ1Z0SkS"
 }
